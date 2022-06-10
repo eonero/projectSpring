d863911386bc124d67efcc06b24927250b7774b0
Vitor 'Nero' Hugo <vitorhugo_guga@hotmail.com>
2022-06-10 18:46:19 -0300

    tratamento de exception

--- src/main/java/com/javaproject/course/service/UsuarioService.java
+++ src/main/java/com/javaproject/course/service/UsuarioService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,9 +47,13 @@ public class UsuarioService {
     }
 
     public Usuario uptade(Long id, Usuario obj){
-        Usuario entity = repository.getOne(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            Usuario entity = repository.getOne(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
 
     }
 
